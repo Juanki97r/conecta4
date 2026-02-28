5de97b4417845460a72606cd122466da73adab2c
Juanki97r <juanki97r@gmail.com>
2026-02-28 21:06:53 +0100

    construir el main

--- src/main/java/daw/Juego.java
+++ src/main/java/daw/Juego.java
@@ -45,7 +45,48 @@ public class Juego {
        
     }
 
+    public boolean comprobarFichas(Juego juego, char jugador,int fila, int columna,  int direccionFila,int direccionColumna){
 
+        int contadorVictoria =1;
+        //comprobar hacia delante
+        int f = fila + direccionFila;
+        int c = columna +direccionColumna;
+
+        //hacia delante
+        while(f>=0 && f< FILAS &&
+            c>=0 && c< COLUMNAS &&
+            juego.getTablero()[f][c]== jugador){
+
+                contadorVictoria++;
+                f+=direccionFila;
+                c+=direccionColumna;
+            }
+        
+        //hacia atras
+        f= fila-direccionFila;
+        c= columna-direccionColumna;
+
+
+             while(f>=0 && f< FILAS &&
+            c>=0 && c< COLUMNAS &&
+            juego.getTablero()[f][c]== jugador){
+
+                contadorVictoria++;
+                f-=direccionFila;
+                c-=direccionColumna;
+            }
+
+
+        return contadorVictoria>=4;
+
+    }
+
+    public boolean comprobarVictoria(Juego juego, char jugador, int fila, int columna){
+        return comprobarFichas(juego, jugador, fila, columna,0,1) || //horizontal
+            comprobarFichas(juego, jugador, fila, columna, 1,0) || //vertical
+            comprobarFichas(juego, jugador, fila, columna, 1,1) || //diagonal descendente
+            comprobarFichas(juego, jugador, fila, columna, 1,-1); //diagonal ascendente
+    }
 
     public int getFILAS() {
         return FILAS;
--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -6,15 +6,10 @@ public class Main {
         Juego conecta4 = new Juego();
         char jugador1='X';
         char jugador2='O';
-        try{
-        conecta4.colocar(3, jugador2); 
-        } catch(IndexOutOfBoundsException ieoe){
-            System.out.println("La columna esta llena");
-        }
-        conecta4.colocar(4, jugador1);
-        conecta4.colocar(4, jugador2);
-        conecta4.colocar(3, jugador1);
-        conecta4.imprimir();
+
+        
+        
+       
 
     }
 }
